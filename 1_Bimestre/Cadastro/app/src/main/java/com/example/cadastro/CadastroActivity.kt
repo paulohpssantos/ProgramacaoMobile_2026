b9e149e8507baa72ed1d71c8292a5798daa5015c
Paulo Henrique Pereira dos Santos <84387138+paulohpssantos@users.noreply.github.com>
2026-03-30 22:28:08 -0300

    adicionado tela de cadastro

--- 1_Bimestre/Cadastro/app/src/main/java/com/example/cadastro/CadastroActivity.kt
+++ 1_Bimestre/Cadastro/app/src/main/java/com/example/cadastro/CadastroActivity.kt
@@ -2,19 +2,31 @@ package com.example.cadastro
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.cadastro.model.Aluno
+import com.example.cadastro.repository.AlunoRepository
 
 class CadastroActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
 
-        val btFechar = findViewById<Button>(R.id.btFechar)
-        btFechar.setOnClickListener {
+        val edRaAluno = findViewById<EditText>(R.id.edRaAluno)
+        val edNomeAluno = findViewById<EditText>(R.id.edNomeAluno)
+        val btSalvar = findViewById<Button>(R.id.btSalvar)
+        btSalvar.setOnClickListener {
+
+            val ra = edRaAluno.text.toString()
+            val nome = edNomeAluno.text.toString()
+            val aluno = Aluno(ra, nome)
+
+            AlunoRepository.listaAlunos.add(aluno)
+
             finish()
         }
 
--- 1_Bimestre/Cadastro/app/src/main/java/com/example/cadastro/MainActivity.kt
+++ 1_Bimestre/Cadastro/app/src/main/java/com/example/cadastro/MainActivity.kt
@@ -13,12 +13,27 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cadastro.adapter.AlunoAdapter
+import com.example.cadastro.repository.AlunoRepository
 import com.example.cadastro.ui.theme.CadastroTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var adapterAlunos : AlunoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        //Carrega a lista de alunos no recycler view
+        val rvAlunos = findViewById<RecyclerView>(R.id.rvAlunos)
+
+        adapterAlunos = AlunoAdapter(AlunoRepository.listaAlunos)
+
+        rvAlunos.layoutManager = LinearLayoutManager(this)
+        rvAlunos.adapter = adapterAlunos
 
         val btAbrirTela = findViewById<Button>(R.id.btAbrirTela)
 
@@ -26,7 +41,11 @@ class MainActivity : ComponentActivity() {
             val intent = Intent(this, CadastroActivity::class.java)
             startActivity(intent)
         }
-
+    }
+    //atualiza a lista ao voltar na tela
+    override fun onResume() {
+        super.onResume()
+        adapterAlunos.notifyDataSetChanged()
     }
 }
 
