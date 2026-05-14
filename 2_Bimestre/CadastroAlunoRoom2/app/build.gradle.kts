38e4c8d01bc0efe86e9474e9d4bd8e29cb06d140
Paulo Henrique Pereira dos Santos <84387138+paulohpssantos@users.noreply.github.com>
2026-05-14 17:20:16 -0300

    fontes

--- 2_Bimestre/CadastroAlunoRoom2/app/build.gradle.kts
+++ 2_Bimestre/CadastroAlunoRoom2/app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("org.jetbrains.kotlin.kapt")
+    id("com.google.devtools.ksp")
 
 
 }
@@ -58,7 +58,7 @@ dependencies {
     // Room
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
 
     // Lifecycle
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
--- 2_Bimestre/CadastroAlunoRoom2/app/src/main/java/com/example/cadastroalunoroom/MainActivity.kt
+++ 2_Bimestre/CadastroAlunoRoom2/app/src/main/java/com/example/cadastroalunoroom/MainActivity.kt
@@ -1,47 +1,73 @@
 package com.example.cadastroalunoroom
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.cadastroalunoroom.ui.theme.CadastroAlunoRoomTheme
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cadastroalunoroom.adapter.AlunoAdapter
+import com.example.cadastroalunoroom.database.AppDatabase
+import com.example.cadastroalunoroom.database.DatabaseProvider
+import com.example.cadastroalunoroom.entity.AlunoEntity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var alunoAdapter: AlunoAdapter
+    private lateinit var db: AppDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CadastroAlunoRoomTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.main_activity)
+
+        //carregando os componentes da tela
+        val recyclerViewAluno = findViewById<RecyclerView>(R.id.rvALunos)
+        val btAdicionarAluno = findViewById<Button>(R.id.btAdicionarAluno)
+
+        //carregando a base de dados
+        db = DatabaseProvider.getDatabase(this)
+
+        //carregar o adapter
+        alunoAdapter = AlunoAdapter(emptyList())
+        recyclerViewAluno.layoutManager = LinearLayoutManager(this)
+        recyclerViewAluno.adapter = alunoAdapter
+
+        carregarAlunos()
+
+        btAdicionarAluno.setOnClickListener {
+            adicionarAluno()
+        }
+
+    }
+
+    private fun carregarAlunos() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val lista = db.alunoDao().listarTodos()
+
+            withContext(Dispatchers.Main){
+                alunoAdapter.atualizarLista(lista)
             }
+        }
+    }
+
+    private fun adicionarAluno(){
+        CoroutineScope(Dispatchers.IO).launch {
+
+            val novoAluno = AlunoEntity(ra = 123, nome = "Juca")
+            val novoAluno1 = AlunoEntity(ra = 456, nome = "Maria")
+            val novoAluno2 = AlunoEntity(ra = 678, nome = "Jose")
+
+            //gravando aluno na tabela do banco de dados
+            db.alunoDao().inserir(novoAluno)
+            db.alunoDao().inserir(novoAluno1)
+            db.alunoDao().inserir(novoAluno2)
+
+            //carregar a lista de alunos
+            carregarAlunos()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CadastroAlunoRoomTheme {
-        Greeting("Android")
-    }
-}
